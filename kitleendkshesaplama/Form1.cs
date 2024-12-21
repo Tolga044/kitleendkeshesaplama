c9d8bb8d7946673b8d4d76f460bca2079f295491
Tolga D. Pektaş <119101966+Tolga044@users.noreply.github.com>
2024-12-21 21:52:11 +0300

    Update Form1.cs

--- kitleendkshesaplama/Form1.cs
+++ kitleendkshesaplama/Form1.cs
@@ -17,26 +17,7 @@ namespace kitleendkshesaplama
             InitializeComponent();
         }
        
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-          
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void textBox3_TextChanged(object sender, EventArgs e)
-        {
-           
-            
-        }
+    
 
         private void button1_Click(object sender, EventArgs e)
         {
